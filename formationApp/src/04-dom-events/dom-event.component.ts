b6a7f1079d40430bbde8fc9f0d4524bab0b32924
Bryce MARCOL <bryce.marcol@sncf.fr>
2024-05-28 11:47:13 +0200

    with play-pause, wheel and dragndrop

--- formationApp/src/04-dom-events/dom-event.component.ts
+++ formationApp/src/04-dom-events/dom-event.component.ts
@@ -1,27 +1,46 @@
-import { Component, Pipe, PipeTransform } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <video #video>
+    <video #video (click)="onClick(video)" (wheel)="onScroll(video, $event)" (mousemove)="onDrag($event)"
+           (mousedown)="isDragged = true" (mouseup)="isDragged = false">
       <source src="../assets/video.mp4"/>
     </video>
-
-    <!-- Exemple d'utilisation de la variable de template : -->
-    <button (click)="testVariable(video)">Test</button>
   `,
 })
 export class DomEventComponent {
+  isDragged: boolean = false
+  delta: { x: number, y: number } = {x: 0, y: 0}
 
-  testVariable(video: HTMLVideoElement) {
-    console.log(video)
-    if(video.paused) {
+  onClick(video: HTMLVideoElement): void {
+    if (video.paused) {
       video.play()
     } else {
       video.pause()
+    }
+  }
+
+  onScroll(video: HTMLVideoElement, event: WheelEvent): void {
+    if (event.deltaY > 0) {
+      video.currentTime += 1
+    } else {
+      video.currentTime -= 1
+    }
+  }
+
+  onDrag($event: MouseEvent): void {
+    if (!this.isDragged) return
+    const video : HTMLVideoElement | null = document.querySelector('video')
+    const deltaX : number = $event.movementX
+    const deltaY : number = $event.movementY
+    this.delta.x += deltaX
+    this.delta.y += deltaY
+    if (video) {
+      video.style.transform = `translate(${this.delta.x}px, ${this.delta.y}px)`
     }
   }
 }
