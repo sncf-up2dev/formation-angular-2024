4a784227754fbc3fb52258e44750546624dd4b27
Bryce MARCOL <bryce.marcol@sncf.fr>
2024-05-29 16:40:56 +0200

    pipe OK

--- formationApp/src/main.ts
+++ formationApp/src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './demo/app.module';
 import { HtmldomComponent } from './02-binding/htmldom.component';
 import { InterpolationTableauComponent } from './01-interpolation/interpolation-tableau.component';
 import { InterpolationComponent } from './01-interpolation/interpolation.component';
+import {PipeTronqueComponent} from "./06-pipes/exercices/pipe-tronque.component";
 
 //platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
 
 // ou
 
-bootstrapApplication(HtmldomComponent).catch(err => console.error(err));
+bootstrapApplication(PipeTronqueComponent).catch(err => console.error(err));
