0df9fcb8e5032dd451ad9bff601dfc73fbb9dcb1
Bryce MARCOL <bryce.marcol@sncf.fr>
2024-06-06 09:41:19 +0200

    first exercise

--- formationApp/src/demo/colis-details.component.ts
+++ formationApp/src/demo/colis-details.component.ts
@@ -1,12 +1,20 @@
-import { Component } from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {Colis} from "../utils/colis";
 
 @Component({
     selector: 'colis-detail',
     template: `
-    Todo : ColisDetailsComponent
-  `,
+      <div class="box">
+        <div>
+          @if(selectedColis) {
+            {{ selectedColis.details }}
+          } @else {
+            Aucun colis sélectionné
+          }
+        </div>
+      </div>  `,
     styles: ``
 })
 export class ColisDetailsComponent {
-
+    @Input() selectedColis?: Colis = undefined
 }
--- formationApp/src/main.ts
+++ formationApp/src/main.ts
@@ -3,8 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './demo/app.module';
 import { FolderComponent } from './07-component-communication/exercice/folder.component';
 
-//platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
 
 // ou
 
-bootstrapApplication(FolderComponent).catch(err => console.error(err));
+//bootstrapApplication(FolderComponent).catch(err => console.error(err));
